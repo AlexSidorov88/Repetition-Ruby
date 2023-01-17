484071c8dae3bdab7df145feeb205ddb52fd515d
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-01-17 11:11:50 +0100

    Update lesson1.rb

--- lesson1.rb
+++ lesson1.rb
@@ -2,7 +2,7 @@ puts "Enter value in km : "
 
 user_input_in_km = gets.to_f
 
-result_in_miles = user_input_in_km / 1.8
+result_in_miles = user_input_in_km / 1.6
 
 puts "Result in miles: #{result_in_miles}"
 
